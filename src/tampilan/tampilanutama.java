c3c155c7c26c023a66942379b700ae9893500f19
Farhat Mantep Bat Dah <farhatdk98@gmail.com>
2020-07-31 17:44:13 +0700

    MF : Progress

--- src/tampilan/tampilanutama.java
+++ src/tampilan/tampilanutama.java
@@ -36,7 +36,7 @@ public class tampilanutama extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        layanan = new javax.swing.JButton();
         datakader = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
 
@@ -131,12 +131,12 @@ public class tampilanutama extends javax.swing.JFrame {
             }
         });
 
-        jButton4.setBackground(new java.awt.Color(153, 153, 255));
-        jButton4.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        jButton4.setText("Daftar Layanan");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        layanan.setBackground(new java.awt.Color(153, 153, 255));
+        layanan.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        layanan.setText("Daftar Layanan");
+        layanan.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                layananActionPerformed(evt);
             }
         });
 
@@ -164,7 +164,7 @@ public class tampilanutama extends javax.swing.JFrame {
                     .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(datakader, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(layanan, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -175,7 +175,7 @@ public class tampilanutama extends javax.swing.JFrame {
                 .addGap(61, 61, 61)
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(layanan, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(datakader, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -219,11 +219,11 @@ public class tampilanutama extends javax.swing.JFrame {
         dashboard.setVisible(true);
     }//GEN-LAST:event_datakaderActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void layananActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_layananActionPerformed
         this.dispose();
-        tampilandaftar dashboard = new tampilandaftar();
+        menupelayanan dashboard = new menupelayanan();
         dashboard.setVisible(true);
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_layananActionPerformed
 
     /**
      * @param args the command line arguments
@@ -265,7 +265,6 @@ public class tampilanutama extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -273,5 +272,6 @@ public class tampilanutama extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton layanan;
     // End of variables declaration//GEN-END:variables
 }
